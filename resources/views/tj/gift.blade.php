1fb821c798238fe262030bee84c9b91eced00b9d
EC2 Default User <ec2-user@ip-172-31-18-165.us-east-2.compute.internal>
2020-07-07 02:33:21 +0000

    mm

--- resources/views/tj/gift.blade.php
+++ resources/views/tj/gift.blade.php
@@ -10,8 +10,9 @@
     </head>
     <body>
         <div class="header">
-            <img src="{{ asset('image/gift_lp_1.jpg') }}"></img>
+            <img src="{{ asset('image/gift_lp_1.jpg') }}" class="header-img"></img>
             <p>参加希望はコチラ <i class="far fa-hand-point-down"></i></p>
+            <img src="{{ asset('image/gift_lp_app.jpg') }}" class="app"></img>
             <a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 専用アプリをインストール <i class="fas fa-external-link-alt"></i></a>
             <p class="header-bottom">インストール後アプリ内ページを要チェック！</p>
         </div>
@@ -23,9 +24,10 @@
         
         <div class="download">
             <p>参加希望はコチラ <i class="far fa-hand-point-down"></i></p>
+            <img src="{{ asset('image/gift_lp_app.jpg') }}" class="app"></img>
             <a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 専用アプリをインストール <i class="fas fa-external-link-alt"></i></a>
             <p class="download-bottom">インストール後アプリ内ページを要チェック！</p>
-            <img src="{{ asset('image/gift_lp_3.jpg') }}"></img>
+            <img src="{{ asset('image/gift_lp_3.jpg') }}" class="download-img"></img>
         </div>
         
         <div class="suggest">
@@ -51,9 +53,10 @@
         
         <div class="download-footer">
             <p>参加希望はコチラ <i class="far fa-hand-point-down"></i></p>
+            <img src="{{ asset('image/gift_lp_app.jpg') }}" class="app"></img>
             <a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 専用アプリをインストール <i class="fas fa-external-link-alt"></i></a>
             <p class="download-footer-bottom">インストール後アプリ内ページを要チェック！</p>
-            <img src="{{ asset('image/gift_lp_8.jpg') }}"></img>
+            <img src="{{ asset('image/gift_lp_8.jpg') }}" class="download-footer-img"></img>
         </div>
         
         <div class="footer">
